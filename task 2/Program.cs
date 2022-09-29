dbbc96f0fa5343a374805cbc47f38887f941bf04
lev <Dmslmrk@icloud.com>
2022-09-29 09:33:10 +0300

    done

---
+++ task 2/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Задайте размер массива");
+Console.WriteLine("Введите количество строк");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+int [,] GetMatrix(int M,int N)
+{
+    int  [,] Array= new int [M,N];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            Array[i,k]= new Random().Next(10);
+        }
+    }
+    return Array;
+}
+void PrintMatrix(int[,]inputMatrix)
+{
+     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int M = 0; M < inputMatrix.GetLength(1); M++)
+        {
+            Console.Write(inputMatrix[i,M] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetMatrix(rows,columns);
+Console.WriteLine("Массив : ");
+PrintMatrix(resultMatrix);
+Console.Write("Выберите элемент формата O.O: ");
+int NumberRows=Convert.ToInt32(Console.ReadLine());
+int NumberColumns=Convert.ToInt32(Console.ReadLine());
+
+if(NumberRows>=resultMatrix.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else if (NumberColumns>=resultMatrix.GetLength(0))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine(resultMatrix[NumberRows,NumberColumns]);
+}
---
+++ task 3/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.WriteLine("Задайте размер массива");
+Console.WriteLine("Введите количество строк");
+int rows=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+int [,] GetMatrix(int M,int N)
+{
+    int  [,] Array= new int [M,N];
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            Array[i,k]= new Random().Next(10);
+        }
+    }
+    return Array;
+}
+void PrintMatrix(int[,]inputMatrix)
+{
+     for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int M = 0; M < inputMatrix.GetLength(1); M++)
+        {
+            Console.Write(inputMatrix[i,M] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] resultMatrix = GetMatrix(rows,columns);
+Console.WriteLine("Массив : ");
+PrintMatrix(resultMatrix);
+double [] Array= new double [resultMatrix.GetLength(0)];
+
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+    double sum=0;
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        sum=sum+resultMatrix[j,i];
+    }
+    Array[i]=((Math.Round (sum/resultMatrix.GetLength(0),2)));
+}
+double Aver=resultMatrix.GetLength(0);
+
+double [] AverageArray= Array;
+Console.WriteLine(string.Join(" ",  AverageArray));
